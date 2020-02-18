1106cfa2cec74ade68ee4635307914a53f3a5550
Katie Belanger <belangerkat@gmail.com>
2020-02-18 15:08:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -67,16 +67,16 @@ end
 
 def checkout(cart, coupons)
   total = 0
-    i = 0
+  i = 0
 
-    ccart = consolidate_cart(cart)
-    apply_coupons(ccart, coupons)
-    apply_clearance(ccart)
+  ccart = consolidate_cart(cart)
+  apply_coupons(ccart, coupons)
+  apply_clearance(ccart)
 
-    while i < ccart.length do
-      total += items_total_cost(ccart[i])
-      i += 1
-    end
-    total >= 100 ? total * (1.0 - BIG_PURCHASE_DISCOUNT_RATE) : total
+  while i < ccart.length do
+    total += items_total_cost(ccart[i])
+    i += 1
   end
+
+  total >= 100 ? total * (1.0 - BIG_PURCHASE_DISCOUNT_RATE) : total
 end
